5262046d7853b7064cf39fd64409c7e7724148a8
Sadikibenda <Sadiki.benda1@gmail.com>
2023-12-13 18:34:34 -0500

    variable added

--- 00_HelloWorld/Program.cs
+++ 00_HelloWorld/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("My name is Sadiki");
+//Console.WriteLine("My name is Sadiki");

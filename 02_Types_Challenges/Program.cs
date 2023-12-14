eb686cca1956ab21c4ef9505b45180cee97c4558
Sadikibenda <Sadiki.benda1@gmail.com>
2023-12-13 19:20:04 -0500

    Change added

---
+++ 02_Types_Challenges/Program.cs
@@ -0,0 +1,45 @@
+﻿//Basics types challenge
+
+int fiVe;
+int Two = 2;
+Console.WriteLine(Two);
+
+string Sam;
+string Sadiki = "sadiki";
+Console.WriteLine(Sadiki);
+
+Console.WriteLine("Sadiki" + 2);
+
+bool yoo;
+bool Ok = true;
+Console.WriteLine(Ok);
+
+float h;
+float f = 20.9f;
+Console.WriteLine(f);
+
+double a;
+double b = 9.600000d;
+Console.WriteLine(b);
+
+decimal m;
+decimal n = 2.9m;
+Console.WriteLine(n);
+
+ulong o;
+ulong p = 10;
+Console.WriteLine(p);
+
+int year = 2023;
+string stringYear = "2023";
+Console.WriteLine(stringYear);
+
+//convert a string to an integer in C#.
+string year = "2021";
+int yearNumber = int.Parse(year);
+Console.WriteLine(yearNumber);
+
+int anotherNumber = Convert.ToInt32(year);
+Console.WriteLine(anotherNumber);
+
+
